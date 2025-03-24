135b59eb49c4fed4c3645bf214f1ef6b5e82eb67
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2025-03-23 22:33:49 -0700

    add user stats route

---
+++ src/app/api/user_stats/route.tsx
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const userId = searchParams.get("userId");
+
+  if (!userId) {
+    return NextResponse.json({ error: "User ID is required" }, { status: 400 });
+  }
+
+  return NextResponse.json({ message: "Hello, world!" });
+}
